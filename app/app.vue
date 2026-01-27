ca29e87e6890ad7868aaa33fba00dcc9fc0e6585
Estéban Soubiran <e.soubiran25@gmail.com>
2026-01-27 15:15:16 +0100

    content: when using nuxt
    fix #8

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
+const { data: page } = await useAsyncData('/', () => queryCollection('content').path('/').first())
 
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
 </script>
 
 <template>
-  <div>
-    Just Fucking Use Nuxt.
+  <div v-if="page">
+    <UPageBody>
+      <ContentRenderer
+        v-if="page.body"
+        :value="page"
+      />
+    </UPageBody>
   </div>
 </template>
---
+++ content/index.md
@@ -0,0 +1,10 @@
+## When should you use Nuxt?
+
+- Need a static website? Use Nuxt.
+- Need a dynamic website? Use Nuxt.
+- Need a server? Use Nuxt.
+- Building an API? Use Nuxt.
+- Want the best SEO? Use Nuxt.
+- Never know where your code executes? Use Nuxt.
+- Tired of fighting your hosting provider? Use Nuxt.
+- Want a delightful developer experience? Use Nuxt.
