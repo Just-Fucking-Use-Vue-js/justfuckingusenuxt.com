55506383500aa3c421414644407d6701b7c0886a
Estéban <e.soubiran25@gmail.com>
2026-01-27 15:48:29 +0100

    chore: add umami (#11)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,20 @@ export default defineNuxtConfig({
     '@nuxt/content',
   ],
 
+  $production: {
+    app: {
+      head: {
+        script: [
+          {
+            'src': 'https://umami.soubiran.dev/script.js',
+            'defer': true,
+            'data-website-id': '81e883b6-abed-4e6f-8661-135f41d16cfa',
+          },
+        ],
+      },
+    },
+  },
+
   devtools: {
     enabled: false,
   },
